207e8f361ebb9539afc9c6af6ad24ee8ba98ef9e
Adrian <adriancerva2021@gmail.com>
2020-10-26 13:35:54 -0500

    first commit AdaptePattern

---
+++ src/OldCoffeeMachine.java
@@ -0,0 +1,14 @@
+
+public class OldCoffeeMachine {
+
+	public void selectA() {
+		
+		System.out.println("Selected American Coffee");
+	}
+	
+	public void selectB() {
+		System.out.println("Selected Capuchino Coffee");
+		
+	}
+	
+}
